fdb4986569bf8e8e96a51dc73cc4277bc7f92bba
Giorgio Loi <giorgio.loi@studenti.polito.it>
2020-10-30 12:27:57 +0100

    connect db

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ var express = require('express'),
   Article = require('./api/models/articleModel'),//created model loading here
   bodyParser = require('body-parser');
 
-  // mongoose instance connection url connection
+// mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/team5Db', {useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb+srv://Gio:pasta13@cluster0.35e0i.mongodb.net/team5db?retryWrites=true&w=majority');
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
-    next();
-   });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -30,9 +30,9 @@ routes(app); //register the route
 
 app.listen(port);
 
-app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'})
-  });
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+});
 
 
 console.log('team5Backed server started on: ' + port);
